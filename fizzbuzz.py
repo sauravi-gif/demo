d6c6d02a9724185e98a47ac22444cbe9dcd950c4
sauravi-rajbhandari <sauravi.rajbhandari@fuse.ai>
2020-02-17 17:00:21 +0545

    adding

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+import sys
+def fizzbuzz(m):
+    for i in range(n+1):
+        if i%3 : print('fizz')
+        elif i%5 : print('buzz')
+        elif i%3 == 0 and i%5 == 0 : print('fizzbuzz')
+        else: print(i)
+if __name__ == '__main__'
+    fizzbuzz(int(sys.argv[1]))
